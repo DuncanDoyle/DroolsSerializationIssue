dc97c496f86051c4987d2fad096cb0f17f0f8c51
Duncan Doyle <Duncan.Doyle@gmail.com>
2014-07-09 19:04:37 +0200

    Tests can now run in PHREAK and RETEOO mode by selecting the correct Maven profile. Logging now shows if the engine runs in PHREAK or RETEOO. Added JavaDoc to describe the intention of the tests.

--- DroolsSerializationIssue/src/test/java/org/jboss/ddoyle/drools/persistence/DroolsSessionPersistenceServiceTest.java
+++ DroolsSerializationIssue/src/test/java/org/jboss/ddoyle/drools/persistence/DroolsSessionPersistenceServiceTest.java
@@ -22,6 +22,16 @@ import org.kie.api.runtime.rule.FactHandle;
  */
 public class DroolsSessionPersistenceServiceTest {
 
+	/**
+	 * Creates a KieSession, inserts an Event with an @expires, fires all rules, retracts the event, serializes the session, deserializes
+	 * the session and serializes the session again. This throws a NPE at at
+	 * org.drools.core.reteoo.ObjectTypeNode$ExpireJobContextTimerOutputMarshaller.serialize(ObjectTypeNode.java:669). It seems to be caused
+	 * by the retraction of the Event. It seems that the Event is retracted, but the serialized session still has an expiry job with a null
+	 * reference, which causes issues on the second serialization.
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	@Test
 	public void testDoSerializeTwice() throws IOException, InterruptedException {
 		File tempFileOne = File.createTempFile("drools-session", ".tmp");
@@ -49,8 +59,9 @@ public class DroolsSessionPersistenceServiceTest {
 
 			kieSession.fireAllRules();
 
-			/* 
-			 * This delete actually causes the issues. The problem seems to be that the expiry job is still serialized, even though the event has been retracted.
+			/*
+			 * This delete actually causes the issues. The problem seems to be that the expiry job is still serialized, even though the
+			 * event has been retracted.
 			 */
 			kieSession.delete(fhOne);
 
@@ -70,6 +81,14 @@ public class DroolsSessionPersistenceServiceTest {
 		}
 	}
 
+	/**
+	 * Creates a KieSession, inserts an Event with an @expires, fires all rules, retracts the event, serializes the session, deserializes
+	 * the session, moves the pseudoclock beyond the point where the expiration job should fire, and fires all rules. This throws a NPE at
+	 * org.drools.core.common.AbstractWorkingMemory$WorkingMemoryReteExpireAction.execute(AbstractWorkingMemory.java:1524).
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	@Test
 	public void testDoSerializeAndProgressClock() throws IOException, InterruptedException {
 
@@ -118,6 +137,48 @@ public class DroolsSessionPersistenceServiceTest {
 			tempFileOne.delete();
 		}
 
+	}
+
+	/**
+	 * Without serialization, everything is executed correctly. Here we insert an Event, fire all rules, retract the Event, advance the
+	 * clock 10 days (beyond the expiry point) and fireAllRules again. This does not give any errors.
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testDeleteveEventAndProgressClock() throws IOException, InterruptedException {
+
+		long currentTime = new Date().getTime();
+
+		// Setup the session.
+		KieServices kieServices = KieServices.Factory.get();
+		KieContainer kieContainer = kieServices.getKieClasspathContainer();
+		KieSession kieSession = kieContainer.newKieSession("testSession");
+		// Setup the clock
+		PseudoClockScheduler clock = kieSession.getSessionClock();
+		long clockTime = clock.getCurrentTime();
+		if (clockTime < currentTime) {
+			clock.advanceTime(currentTime - clockTime, TimeUnit.MILLISECONDS);
+		} else {
+			throw new IllegalStateException("Clock has already advanced beyond the curreny time.");
+		}
+
+		// Create event and insert into session.
+		SimpleEvent eventOne = new SimpleEvent(currentTime + 1000);
+		FactHandle fhOne = insertEvent(kieSession, eventOne);
+
+		kieSession.fireAllRules();
+
+		/*
+		 * This delete actually causes the issues. The problem seems to be that the expiry job is still serialized, even though the event
+		 * has been retracted.
+		 */
+		kieSession.delete(fhOne);
+
+		// Advance the PseudoClock 10 days so the expiry of the event kicks in. This will also throw a NPE.
+		clock.advanceTime(864000001, TimeUnit.MILLISECONDS);
+		kieSession.fireAllRules();
 	}
 
 	private FactHandle insertEvent(KieSession kieSession, SimpleEvent event) {
